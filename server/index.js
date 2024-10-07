8a49cde9c512fcdaa311fa138bfde6eb3c53e697
Ayush Thakur <zoomgy@Ayushs-MacBook-Air-2.local>
2024-10-07 14:41:17 +0530

    added port

--- server/index.js
+++ server/index.js
@@ -92,6 +92,6 @@ app.post('/logout', (req, res) => {
     res.send({ message: "Successfully logged out" });
 });
 
-app.listen(3000,()=>{
+app.listen(process.env.PORT || 3000,()=>{
     console.log("Listening on port 3000");
 })
